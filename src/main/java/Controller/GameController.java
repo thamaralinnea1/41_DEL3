5c0cf9423659eda3dec8535798993840f9e2efbf
Pernille <pernille.lyngholm1@outlook.com>
2020-11-20 10:59:30 +0100

    GameController created

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -1,4 +1,45 @@
 package Controller;
 
+import Models.Board.Board;
+import Models.Board.Die;
+import Models.Board.Field;
+import Models.Player.Player;
+
 public class GameController {
+
+    public Board board;
+    public Die die;
+
+
+    public GameController() {
+        this.board = new Board();
+        this.die = new Die();
+
+
+
+    }
+    // returnere felt spilleren står på
+    public Field getCurrentField(Player currentPlayer){
+        return this.board.field[currentPlayer.getPosition()];
+    }
+
+    public boolean canBuyField (Player currentPlayer){
+        Field currentField = getCurrentField(currentPlayer);
+        return currentField.getPropertyOwner() == null;
+
+    }
+
+    //Returne om hvis balance er støre end field price
+    public boolean checkAccount (Player currentPlayer, Field currentField) {
+        return currentPlayer.getAccount().getBalance() >= currentField.getPrice();
+
+
+    }
+
+    public void buyField (Player currentPlayer){
+        Field currentField = getCurrentField(currentPlayer);
+        currentField.setPropertyOwner(currentPlayer);
+
+
+    }
 }
