8e68ac142a63761eded1e2b3aed88656aaa48078
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-11-20 10:53:05 +0100

    PlayerController modified

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -1,5 +1,29 @@
 package Controller;
 
+import Models.Player.Player;
+
 public class PlayerController {
+    public Player currentPlayer;
+    public Player[] playerList;
+    int currentPlayerId;
+
+    public PlayerController(int amountOfPlayers) {
+        this.playerList = new Player[amountOfPlayers];
+        this.currentPlayerId = 0;
+
+    }
+
+    //Metode der flytter spiller
+    public void movePlayer (int positon ){
+        this.currentPlayer.setPosition(positon);
+
+    }
+
+    //CurrentPlayer øges med 1. Modules bruges så curretn player ikke kan overskride arraylist.
+    public void switchPlayer(){
+        currentPlayerId = (currentPlayerId+1)%playerList.length;
+        currentPlayer = playerList[currentPlayerId];
+
+    }
 
 }
