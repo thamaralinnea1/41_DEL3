fd7d0644bd2e3b165753ac5b13cb95fd33ccef68
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2020-11-23 21:13:48 +0100

    3

---
+++ src/test/java/Models/Board/DieTest.java
@@ -0,0 +1,20 @@
+package Models.Board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DieTest {
+
+    @Test
+    void roll() {
+    }
+
+    @Test
+    void getFaceValue() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
